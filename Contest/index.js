1151a18057b174b3c0baefd8d9346b38249b8099
Christ Santiago cowdell <91978458+0thedarkweb0@users.noreply.github.com>
2023-05-11 12:18:27 -0600

    post

--- Contest/index.js
+++ Contest/index.js
@@ -1,1 +1,14 @@
-// JavaScript Document
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 5000); // Change image every 5 seconds
+} 
